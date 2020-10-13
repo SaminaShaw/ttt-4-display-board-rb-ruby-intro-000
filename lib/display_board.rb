98fafa5859dc28ea33a0ffac67c2ef255b124575
hilarious-recipe-9721 <saminashaw3@gmail.com>
2020-10-13 22:37:58 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,9 +2,9 @@
 # out the current state.
 board = ["x", "o", "x", "o", "x", "x", "o", "x", "o"]
 def display_board(board)
-  puts "   |   |   "
+  puts " #{x[0]}|#{o[1]}|#{x[2]} "
   puts "-----------"
-  puts "   |   |   "
+  puts " #{o[3]}|#{x[4]}|#{x[5]} "
   puts "-----------"
-  puts "   |   |   "
+  puts "#{o[6]} |#{x[7]}|#{o[8]} "
 end
