f7a5ed96d8d6b4c48c9e7ceeee52b7736a99ba19
hilarious-recipe-9721 <saminashaw3@gmail.com>
2020-10-13 22:43:03 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,10 +1,10 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-board = ["x", "o", "x", "o", "x", "x", "o", "x", "o"]
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def display_board(board)
-  puts " #{x[0]}|#{o[1]}|#{x[2]} "
-  puts "-----------"
-  puts " #{o[3]}|#{x[4]}|#{x[5]} "
-  puts "-----------"
-  puts "#{o[6]} |#{x[7]}|#{o[8]} "
+   puts " #{board[0]} | #{board[1]} | #{board[2]} "
+   puts "-----------"
+   puts " #{board[3]} | #{board[4]} | #{board[5]} "
+   puts "-----------"
+   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
